095577c9030d8d90398c72e89a2f97849d9de924
Isabella Moreira <babellamoreira@gmail.com>
2022-06-19 17:36:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function msg(){
+    console.log("Estudando JS")
+}
+msg()
+
+function meunome(nome){
+    console.log("Me chamo", nome )
+}
+meunome("Isabella")
+
+function quemsoueu (nome, idade, musica){
+    console.log (`Me chamo ${nome} gosto de ouvir ${musica} e tenho ${idade} anos`)
+}
+quemsoueu("Isabella", "32", "reggae")
+
+function musicaefilme (filme, musica){
+    console.log(`Meu filme favorito é ${filme} e agora estou ouvindo ${musica}`)
+}
+musicaefilme("Pulp Fiction", "Fica até umas hora - Black Alien")
